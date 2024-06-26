81f03ae65769d85f746b168b8a8dadedb4627bf4
Otormin <yjesse330@gmail.com>
2024-06-26 15:27:45 +0100

    first-commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Script for navigation bar
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
+const close = document.getElementById("close"); 
+
+bar.addEventListener("click", function(){
+    nav.classList.add("active");
+});
+
+close.addEventListener("click", function(){
+    nav.classList.remove("active");
+});
